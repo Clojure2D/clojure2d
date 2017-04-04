c915e096e7e7a53a16269241f4865c166787814b
tsulej <tomeksul@gmail.com>
2017-04-04 20:53:44 +0200

    pendulum update

--- examples/ex28_double_pendulum.clj
+++ examples/ex28_double_pendulum.clj
@@ -1,3 +1,7 @@
+;; Double pendulum simulation using sicmutils
+;;
+;; https://github.com/littleredcomputer/sicmutils/blob/master/src/sicmutils/examples/double_pendulum.clj
+;; https://mitpress.mit.edu/books/structure-and-interpretation-classical-mechanics-0
 (ns examples.ex28-double-pendulum 
   (:require [sicmutils.examples.double-pendulum :refer :all]
             [sicmutils.structure :as ss]
@@ -8,22 +12,27 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
+;; define canvas, window
 (def canvas (create-canvas 600 600))
 (def ^:const ^long fps 50)
 (def windows (show-window canvas "Double pendulum" 600 600 fps))
 
+;; canvas is refreshed externally by integrator, let's define frame rate
 (def ^:const ^double time-delay (/ 1000.0 fps))
 
-(def ^:const ^double len 0.7) ; length of first rod, second has length (- 1.0 len)
-(def ^:const ^double mass1 15) ; mass of the first ball
-(def ^:const ^double mass2 4) ; mass of the second ball
+;; pendulum settings
+(def ^:const ^double len 0.6) ; length of first rod, second has length (- 1.0 len)
+(def ^:const ^double mass1 4) ; mass of the first ball
+(def ^:const ^double mass2 10) ; mass of the second ball
 (def ^:const ^double theta (- m/PI 0.1)) ; angle of the first rod
 (def ^:const ^double phi (m/drand m/TWO_PI)) ; angle of the second rod
 (def ^:const ^double simulation-time 10.0) ; time of simulation (it's not animation time)
-(def ^:const ^double speed 1.0) ; simulation speed, set to 1.0 to have real time animation
+(def ^:const ^double speed 0.5) ; simulation speed, set to 1.0 to have real time animation
+
 
 (do
 
+  ;; precalculated parameters
   (def ^:const ^double len1 len)
   (def ^:const ^double len2 (- 1.0 len))
 
@@ -33,17 +42,20 @@
   (def ^:const ^double m1 (* 20.0 (m/cbrt mass1)))
   (def ^:const ^double m2 (* 20.0 (m/cbrt mass2)))
 
+  ;; local drawing buffer
   (def local-canvas (create-canvas 600 600))
-  
+
   (defn observe
-    ""
-    [t ^Struct state]
-    (let [[a0 a1] (ss/structure->vector ((.v state) 1))
-          posx1 (+ 300 (* l1 (m/sin a0)))
-          posy1 (+ 300 (* l1 (m/cos a0)))
+    "StepHandler callback function (see org.apache.commons.math3.ode.nonstiff.GraggBulirschStoerIntegrator)"
+    [_ ^Struct state]
+    (let [[a0 a1] (ss/structure->vector ((.v state) 1)) ;; current position (angles)
+          ;; polar to cartesian
+          posx1 (+ 300.0 (* l1 (m/sin a0)))
+          posy1 (+ 300.0 (* l1 (m/cos a0)))
           posx2 (+ posx1 (* l2 (m/sin a1)))
           posy2 (+ posy1 (* l2 (m/cos a1)))]
-      
+
+      ;; draw rods, balls on buffer
       (with-canvas local-canvas
         (set-color 0 0 0 220)
         (rect 0 0 600 600)
@@ -56,11 +68,14 @@
         (ellipse posx1 posy1 m1 m1)
         (ellipse posx2 posy2 m2 m2))
 
+      ;; draw on window
       (with-canvas canvas
         (image (@local-canvas 1)))
-      
+
+      ;; wait
       (Thread/sleep time-delay)))
 
+  ;; run integration
   (evolver {:t simulation-time
             :dt (* speed (/ 1.0 fps))
             :l1 len1
