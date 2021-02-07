1ac8d177684eefc0583d0278dae004365d08e536
GenerateMe <generateme.blog@gmail.com>
2021-02-08 00:06:49 +0100

    1.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
+## 1.4.1
+
+### Changed
+
+* deps updated
+
 ## 1.4.0
 
 ### Added
--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clojure2d "1.4.0"
+(defproject clojure2d "1.4.1"
   :description "Creative coding / glitch library backed by Java2D"
   :url "https://github.com/Clojure2D/clojure2d"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [generateme/fastmath "2.0.3"]
-                 [org.apache.xmlgraphics/batik-transcoder "1.13"]]
+                 [generateme/fastmath "2.1.0"]
+                 [org.apache.xmlgraphics/batik-transcoder "1.14"]]
   :resource-path "resources/"
   :java-source-paths ["src"]
   :javac-options ["-target" "1.8" "-source" "1.8"]
