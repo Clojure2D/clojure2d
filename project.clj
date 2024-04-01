a0dd967bfbf2ac1782711c3b5c17bd48d233fdba
GenerateMe <generateme.blog@gmail.com>
2024-04-01 17:09:52 +0200

    deps up

--- project.clj
+++ project.clj
@@ -4,12 +4,11 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [generateme/fastmath "2.2.2-SNAPSHOT" :exclusions [com.github.haifengl/smile-mkl]]
+                 [generateme/fastmath "2.4.0" :exclusions [com.github.haifengl/smile-mkl]]
                  [com.scrtwpns/mixbox "2.0.0"]
-                 [org.apache.xmlgraphics/batik-transcoder "1.16"]]
+                 [org.apache.xmlgraphics/batik-transcoder "1.17"]]
   :resource-path "resources/"
   :java-source-paths ["src"]
-  ;; :javac-options ["-target" "1.8" "-source" "1.8"]
   :javac-options ["--release" "8"]
   :prep-tasks [["compile" "fastmath.vector"] "javac"]
   :scm {:name "git"
@@ -17,7 +16,7 @@
   :repl-options {:timeout 120000}
   :target-path "target/%s"
   :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-             :eastwood {:plugins [[jonase/eastwood "1.4.0"]]
+             :eastwood {:plugins [[jonase/eastwood "1.4.2"]]
                         :eastwood {:add-linters [:performance :boxed-math]
                                    :exclude-namespaces [notebooks.color]}}
              :dev {:source-paths ["dev"]
